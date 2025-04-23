9ff4a4c5b3ae1008ee69aeec18b2c33bcb79ac18
Yasin5949 <yasin2ashalo@gmail.com>
2025-04-23 00:31:33 -1200

    Added Admin Dashbord and Made some function to service page

--- Backend/register.php
+++ Backend/register.php
@@ -1,2 +1,14 @@
 <?php
 require 'dbConnection.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $Fname = $_POST['Fname'];
+    $Lname = $_POST['Lname'];
+    $Email = $_POST['email'];
+    $Password = Password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $Confirm = $_POST['confirm'];
+    $Role = 2;
+}
+if ($Password === $Confirm) {
+    $sql = "insert into users(FirstName,LastName,Email,hashedPassword,RoleId)
+    values('$Fname','$Lname','$Email','$Password','$Role')";
+}
--- Javascripts/Service.js
+++ Javascripts/Service.js
@@ -24,4 +24,56 @@ function Menu(){
 function Exit(){
        document.querySelector('.menu').style.height='0';
        document.querySelector('.menu').innerHTML=``;
-}   
+}  
+function unSelect(){
+    document.querySelectorAll('.service').forEach((button)=>{
+        button.classList.remove('selected');
+    })
+} 
+document.querySelectorAll('.service').forEach((button)=>{
+    button.addEventListener('click',()=>{
+    unSelect();
+    button.classList.add('selected');
+})
+})
+function Book(){
+    document.querySelector('.displayContainer').innerHTML=`
+        <div class="booked">
+            <div class="placeImage ">
+                <img src="../images/abay.jpg" alt="">
+            </div>
+            <div class="placeName">ABAY</div>
+            <div class="note"></div>
+        </div>
+        <div class="detailsContainer">
+            <div class="details">
+                <div class="bookedDate">Booked ON: 12/12/2025</div>
+                <div class="goingDate">Going ON: 12/12/2025</div>
+                <button class="button">Change Date</button>
+                <div class="dateEdit">
+                    <input type="date">
+                    <button>Apply</button>
+                </div>
+            </div>
+            <div class="bookedHotel"></div>
+        </div><div class="booked">
+            <div class="placeImage ">
+                <img src="../images/abay.jpg" alt="">
+            </div>
+            <div class="placeName">ABAY</div>
+            <div class="note"></div>
+        </div>
+        <div class="detailsContainer">
+            <div class="details">
+                <div class="bookedDate">Booked ON: 12/12/2025</div>
+                <div class="goingDate">Going ON: 12/12/2025</div>
+                <button class="button">Change Date</button>
+                <div class="dateEdit">
+                    <input type="date">
+                    <button>Apply</button>
+                </div>
+            </div>
+            <div class="bookedHotel"></div>
+        </div>
+    `;
+}
